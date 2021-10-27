17b87528854a865df0ee95502c0a084f02b8eb57
bobor16 <bobor16@student.sdu.dk>
2021-10-27 10:28:03 +0200

    first commit

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+
+//declare all required constants at one place
+object Constants {
+    const val REQUEST_CODE_ACTIVITY_TRANSITION = 123
+    const val REQUEST_CODE_INTENT_ACTIVITY_TRANSITION = 122
+    const val ACTIVITY_TRANSITION_NOTIFICATION_ID = 111
+    const val ACTIVITY_TRANSITION_STORAGE = "ACTIVITY_TRANSITION_STORAGE"
+}
